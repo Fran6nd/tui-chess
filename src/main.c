dc77eeecead74895695326ca3d247b086bdb09a4
François NOUAILLE DEGORCE <nouaille.degorce.francois@gmail.com>
2020-08-12 00:11:37 +0200

    src/main.c: working on mouse implementation.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include <ncurses.h>
 #include <locale.h>
+#include <ncurses.h>
 #include <stdlib.h>
 
 #include "playground.h"
@@ -15,6 +15,11 @@ char get_team(char id) {
 int main(int argc, char *argv[]) {
   setlocale(LC_ALL, "");
   initscr();
+  cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+  mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
+  printf("\033[?1003h\n");
   curs_set(0);
   if (has_colors() == FALSE) {
     endwin();
@@ -26,9 +31,22 @@ int main(int argc, char *argv[]) {
   init_pair(WHITE_TILE, COLOR_BLACK, COLOR_WHITE);
 
   plg_playground plg = plg_new();
-  plg_draw(&plg);
 
-  getch();
+  int c;
+  do {
+    plg_draw(&plg);
+    c = wgetch(stdscr);
+    if (c != ERR) {
+      if (c == KEY_MOUSE) {
+        MEVENT event;
+        if (getmouse(&event) == OK) {
+            move(event.y, event.x);
+            addch('x');
+        } else {
+        }
+      }
+    }
+  } while (c != (int)'q');
   endwin();
 
   return EXIT_SUCCESS;
